86922fc714d63d616f48a8c3400f1807ffdcbb59
camilagarcia36 <78553396+camilagarcia36@users.noreply.github.com>
2023-04-01 20:34:36 +0200

    cambios almacenes

--- Ejercicios3_almacenes.sql
+++ Ejercicios3_almacenes.sql
@@ -4,14 +4,14 @@ USE `ejerciciotres`;
 
 DROP TABLE IF EXISTS `almacenes`;
 CREATE TABLE `almacenes` (
-  `CODIGO` int NOT NULL,
-  `LUGAR` varchar(255) NOT NULL,
+  `CODIGO` int NOT NULL AUTO_INCREMENT,
+  `LUGAR` varchar(100) NOT NULL,
   `CAPACIDAD` int NOT NULL,
   PRIMARY KEY (`CODIGO`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `almacenes` VALUES (1,'Valencia',3),(2,'Barcelona',4),(3,'Bilbao',7),(4,'Los Angeles',2),(5,'San Francisco',8);
-INSERT INTO 'almacenes' VALUES (1, 'Madrid', 500),(2, 'Barcelona', 400),(3, 'Valencia', 300),(4, 'Sevilla', 200),(5, 'Bilbao', 100);
+INSERT INTO `almacenes` VALUES ('Valencia',3),('Barcelona',4),('Bilbao',7),('Los Angeles',2),('San Francisco',8);
+INSERT INTO 'almacenes' VALUES ('Madrid', 500),('Barcelona', 400),('Valencia', 300),('Sevilla', 200),('Bilbao', 100);
 
 
 
@@ -22,8 +22,7 @@ CREATE TABLE `cajas` (
   `VALOR` double NOT NULL,
   `ALMACEN` int NOT NULL,
   PRIMARY KEY (`NUMREFERENCIA`),
-  KEY `ALMACEN` (`ALMACEN`),
-  CONSTRAINT `cajas_ibfk_1` FOREIGN KEY (`ALMACEN`) REFERENCES `almacenes` (`CODIGO`)
+  CONSTRAINT FK_ALMACENES FOREIGN KEY (ALMACEN) REFERENCES almacenes(CODIGO)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `cajas` VALUES ('0MN7','Rocks',180,3),('4H8P','Rocks',250,1),('4RT3','Scissors',190,4),('7G3H','Rocks',200,1),('8JN6','Papers',75,1),('8Y6U','Papers',50,3),('9J6F','Papers',175,2),('LL08','Rocks',140,4),('P0H6','Scissors',125,1),('P2T6','Scissors',150,2),('TU55','Papers',90,5);
